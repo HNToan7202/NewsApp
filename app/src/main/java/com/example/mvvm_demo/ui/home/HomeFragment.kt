062d28c03acca3759725dd9e0530950ab7bda230
Nguyễn Hoàng Toàn <toannguyen7202@gmail.com>
2025-06-15 22:35:41 +0700

    push code v3

--- app/src/main/java/com/example/mvvm_demo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mvvm_demo/ui/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.example.mvvm_demo.ui.home
 
+import androidx.navigation.NavArgs
+import androidx.navigation.fragment.navArgs
 import com.example.mvvm_demo.R
 import com.example.mvvm_demo.databinding.FragmentHomeBinding
 import com.example.mvvm_demo.ui.base.BaseFragment
@@ -12,6 +14,8 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
     override fun viewModelClass(): Class<HomeViewModel> = HomeViewModel::class.java
 
     override fun initView() {
+        val arg: HomeFragmentArgs by navArgs()
+        val name = arg.userName
     }
 
 }
